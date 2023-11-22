73df7dc11611592bd54c9b6f46570e76996e6f7e
StrategicWebAdmin <tusharhoque4@gmail.com>
2023-11-21 23:48:19 -0500

    adding all basic things for the site

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+ //= link banner_final.mp4
+  //= link hero-banner.mp4
